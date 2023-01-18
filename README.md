78ec01a9b509c56b1177945ce9d1c23ec1dbadaf
willian-rattis <willian.rattis@hotmail.com>
2023-01-18 01:37:34 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# README
+
+## Descrição geral
+Este é meu portfólio pessoal, nele você vai encontrar alguns projetos que desenvolvi utilizando HTML, CSS e JavaScript. Ele foi desenvolvido usando o Visual Studio Code.
+
+## Tecnologias usadas
+- HTML
+- CSS
+- JavaScript
+- Visual Studio Code
+
+## Instruções de uso
+- Para navegar pelo site, basta rolar a página.
+- Caso algum componente não carregue, pressione F5 para recarregar a página.
+
+## Contato
+- Github: https://github.com/willianrattis
+- LinkedIn: https://www.linkedin.com/in/willianrattis/
+- E-Mail: Willian.rattis@gmail.com
+
+## Créditos
+- Aos repositórios públicos do Github que me deram muita inspiração e força para continuar mesmo não sendo muito criativo com front-end.
+
+## Licença
+- Este projeto está sob licença pública aberta.
---
+++ db/db.js
@@ -0,0 +1,461 @@
+export default {
+	bio: {
+		about: {
+			text: [
+				"Olá &#128075;",
+				"Meu nome é Willian. Atualmente eu trabalho como Engenheiro de Software especializado em NET. Graduado pela FATEC. Eu sou um desenvolvedor, curioso, gosto de aprender, trabalhar em equipe e propor soluções. Tenho um pefil proativo.",
+				"Eu sou um desenvolvedor, curioso, gosto de aprender, trabalhar em equipe e propor soluções. Tenho um pefil proativo.",
+				"Adoro café e pretendo experimentar todos.",
+			],
+		},
+		contact: {
+			text: [
+				"Se você quiser entrar em contato comigo, seja para explorar uma tecnologia, um negócio ou apenas dizer oi, sinta-se livre para me enviar um e-mail em willian.rattis@gmail.com",
+			],
+		},
+	},
+	skills: [
+		{
+			title: "Languages",
+			skillName: "Visual C#",
+			color: "1",
+			percentage: "88",
+		},
+		{
+			title: "Frameworks/Libraries",
+			skillName: "Angular, Reactjs",
+			color: "2",
+			percentage: "30",
+		},
+		{
+			title: "Backend",
+			skillName: "Nodejs, MongoDB",
+			color: "3",
+			percentage: "40",
+		},
+		{
+			title: "Clouds",
+			skillName: "Azure",
+			color: "4",
+			percentage: "30",
+		},
+		{
+			title: "Design",
+			skillName: "HTML, Bootstrap, SCSS",
+			color: "5",
+			percentage: "70",
+		},
+		{
+			title: "Version Control",
+			skillName: "GitHub",
+			color: "6",
+			percentage: "70",
+		},
+		{
+			title: "Tools",
+			skillName: "Postman",
+			color: "7",
+			percentage: "82",
+		},
+		{
+			title: "Saas products",
+			skillName: "JIRA, Trello",
+			color: "8",
+			percentage: "50",
+		},
+		{
+			title: "Editor",
+			skillName: "VS Code",
+			color: "9",
+			percentage: "77",
+		},
+	],
+	projects: {
+		web: [
+			{
+				projectName: "",
+				image: "",
+				summary:
+					"",
+				preview: "",
+				techStack: [
+					// "ASP.NET MVC",
+					// "HTML5",
+					// "Bootstrap",
+					// "JavaScript",
+					// "SCSS",
+				],
+			}
+		],
+		software: [
+			{
+				projectName: "",
+				image: "",
+				summary:
+					"",
+				preview:
+					"",
+				techStack: [
+					// "NET",
+					// "ASP.NET Web API",
+					// "Swagger",
+					// "Microserviços",
+					// "Kafka",
+					// "MongoDb",
+				],
+			}
+		],
+		frontend: [
+			{
+				projectName: "",
+				image: "",
+				summary:
+					"",
+				techStack: [
+					// "JavaScript",
+					// "HTML5",
+					// "CSS",
+					// "React",
+					// "Angular"
+				],
+			}
+		]
+	},
+	experience: [
+		{
+			title: "VIA S.A",
+			duration: "Dezembro 2022 - Presente",
+			subtitle: "Software Engineer III",
+			details: [
+				"Como desenvolvedor tive a oportunidade de construir sistemas escaláveis para o setor varejista, lidero equipes na análise, implementação e manutenção de aplicativos para Android e iOS, garantindo alta performance e escalabilidade. Utilizo ferramentas de teste de regressão, como Newman, para garantir a qualidade do software antes de sua implementação em produção.",
+				"Neste projeto, utilizamos as seguintes tecnologias: Visual Studio Code, Git, ASP.NET Core (WEB API, Minimal APIs, Console, Packages), NET (6, 7), AKS, Auto Mapper, Git, JWT, MOQ, NLog, Polly, Prometheus, Redis, Refit, Swagger, WireMock .NET e xUnit .NET",
+				"Neste projeto utilizamos as ferramentas Visual Studio Code, Git, ASP.NET Core (WEB API) e NET Core (3.0, 5.0) para desenvolver soluções escaláveis e performáticas. Utilizo AKS para o deploy e gerenciamento de clusters de Kubernetes, e bibliotecas como Auto Mapper, C# 9.0 e 10, JWT, MOQ, NLog, Polly, Prometheus, Redis, Refit, Swagger, WireMock .NET e xUnit .NET para garantir a qualidade e testabilidade do meu código."
+			],
+			tags: [
+				"Visual C#",
+				"NET 6",
+				"Docker",
+				"Kubernetes",
+				"Microserviços",
+				"Github Actions",
+			],
+			icon: "shopping-bag",
+		},
+		{
+			title: "Stefanini Consultoria e Assessoria S.A",
+			duration: "Agosto 2021 - Dezembro 2022",
+			subtitle: "Software Engineer",
+			details: [
+				"Como desenvolvedor em trabalhei com serviços distribuídos para o setor varejista, desenvolvendo APIs Gateway para atender aplicativos Mobile e SPAs. Meus projetos incluem a manutenção contínua dos serviços, garantindo alta disponibilidade e escalabilidade. Utilizo ferramentas de documentação, como OpenAPI (Swagger) e Postman, para garantir a clareza e facilidade de uso das APIs.",
+				"Neste projeto utilizamos as ferramentas Visual Studio Code, Git, ASP.NET Core (WEB API) e NET Core (3.0, 5.0) para desenvolver soluções escaláveis e performáticas. Utilizo AKS para o deploy e gerenciamento de clusters de Kubernetes, e bibliotecas como Auto Mapper, C# 8.0 e 9, JWT, MOQ, NLog, Polly, Prometheus, Redis, Refit, Swagger, WireMock .NET e xUnit .NET para garantir a qualidade e testabilidade do meu código.",
+			],
+			tags: [
+				"Visual C#",
+				"NET 5",
+				"Docker",
+				"Kubernetes",
+				"Microserviços",
+				"Jenkins",
+			],
+			icon: "shopping-bag",
+		},
+		{
+			title: "MOUT'S",
+			duration: "Janeiro 2020 - Junho 2021",
+			subtitle: "Desenvolvedor Sênior",
+			details: [
+				"Como desenvolvedor, trabalhei na equipe responsável pela migração dos sistemas de gestão de insumos na fábrica da AMBEV, de PHP 7 para ASP.NET Core. Essa migração permitiu uma melhoria significativa no desempenho dos sistemas, além de garantir uma maior segurança e manutenibilidade das aplicações.",
+				"A migração do sistema de NET Core 2.2 para o 3.0 foi um desafio significativo, devido às grandes mudanças no framework. Foi necessário ajustar o código para se adequar às novas funcionalidades e melhorias, mas graças ao esforço da equipe de desenvolvimento, conseguimos realizar essa tarefa com sucesso.",
+			],
+			tags: [
+				"Visual C#",
+				"NET 2.2",
+				"NET 3.0",
+				"PHP",
+				"Docker",
+				"Apache",
+			],
+			icon: "beer",
+		},
+		{
+			title: "CESTECH",
+			duration: "Novembro 2018 - Julho 2019",
+			subtitle: "Desenvolvedor Sênior",
+			details: [
+				"Como desenvolvedor trabalhei na manutenção de sistemas, com equipes no aperfeiçoamento de soluções tecnológicas personalizadas para prefeituras e sistemas estaduais.",
+				"Meus projetos incluem a criação de aplicativos e sistemas web para garantir a eficiência e transparência dos processos administrativos, além de manter a integridade e segurança dos dados. Acompanhando de perto a evolução e tendências tecnológicas para garantir a melhor performance para os sistemas.",
+			],
+			tags: [
+				"Angular",
+				"Visual C#",
+				"NET 3.0",
+				"SQL Server",
+				"ASP.NET MVC",
+				"TFS",
+			],
+			icon: "users",
+		},
+		{
+			title: "AGÊNCIA ROCK",
+			duration: "Julho 2017 - Novembro 2018",
+			subtitle: "Desenvolvedor Pleno",
+			details: [
+				"Como desenvolvedor, tive a oportunidade de atuar em projetos de e-commerce, desenvolvimento de APIs, integração com arquivos XML, criação de catálogos de prêmios e campanhas de incentivo interno para empresas renomadas como BRASTEMP, CONSUL, PETROBRAS e SAMSUNG. Tudo isso me permitiu adquirir uma ampla gama de habilidades e conhecimentos na área de marketing digital, e me permitiu entregar soluções eficazes para meus clientes."
+			],
+			tags: [
+				"HTML5",
+				"Bootstrap",
+				"Visual C#",
+				"NET 2.2",
+				"ASP.NET MVC",
+				"Git",
+			],
+			icon: "bullhorn",
+		},
+		{
+			title: "GS RETAIL",
+			duration: "Janeiro 2015 - Julho 2017",
+			subtitle: "Desenvolvedor Pleno",
+			details: [
+				"Como desenvolvedor de software, pude trabalhar em soluções para o setor varejista, visando a melhoria da gestão e auxílio aos varejistas. Utilizei diversas tecnologias para alcançar esses objetivos, incluindo C#, ASP.NET, JavaScript, CSS, HTML5, Web Services e integrações de sistemas. Isso me permitiu criar soluções robustas e personalizadas para atender às necessidades específicas dos meus clientes do varejo."
+			],
+			tags: [
+				"HTML5",
+				"CSS",
+				"Visual C#",
+				"NET Framework 4.5",
+				"ASP.NET",
+				"SQL Server",
+			],
+			icon: "shopping-bag",
+		},
+		{
+			title: "Kemek Soluções",
+			duration: "Janeiro 2014 - Outubro 2017",
+			subtitle: "Desenvolvedor Júnior",
+			details: [
+				"Como desenvolvedor de software, nesta época trabalhei com desenvolvimento de sistemas web utilizando C# e ASP.NET MVC com Razor para o front-end. Isso me permitiu criar soluções eficientes e intuitivas para usuários, com uma interface amigável e fácil de usar. Além disso, a experiência adquirida na construção de aplicações web me permitiu me desenvolver como profissional e aprender mais sobre arquitetura MVC."
+			],
+			tags: [
+				"HTML5",
+				"CSS",
+				"Visual C#",
+				"NET Framework 4.0",
+				"ASP.NET MVC",
+			],
+			icon: "shopping-bag",
+		},
+		{
+			title: "Grupo SHC S.A",
+			duration: "Março 2013 - Novembro 2013",
+			subtitle: "Desenvolvedor Júnior",
+			details: [
+				"Como desenvolvedor de software, tive a oportunidade de trabalhar com sistemas ERP e desenvolvimento de sistemas web para a recém-chegada montadora JAC Motors. Essa experiência me permitiu trabalhar com soluções que contribuíam para as atividades comuns necessárias para concessionárias da marca, incluindo gerenciamento de estoque, vendas e relatórios financeiros. Isso me permitiu adquirir habilidades valiosas e conhecimentos sobre a indústria automotiva, bem como melhorar minha compreensão de como fazer levantamento de requisitos."
+			],
+			tags: [
+				"HTML5",
+				"CSS",
+				"Visual C#",
+				"NET Framework 4.0",
+				"ASP.NET MVC",
+			],
+			icon: "car",
+		},
+		{
+			title: "Print Laser Service S.A",
+			duration: "Janeiro 2012 - Março 2013",
+			subtitle: "Desenvolvedor Júnior",
+			details: [
+				"Como desenvolvedor de software, tive a oportunidade de trabalhar com tratamento de dados para uma das maiores empresas do ramo de impressão de correspondências e contas do país. Essa foi minha primeira experiência e foi muito valiosa, pois devido ao grande volume de dados a serem tratados, tive a oportunidade de trabalhar com técnicas avançadas de manipulação de strings, expressões regulares e desenvolvimento de serviços para Windows que rodam em background nos servidores, aguardando a chegada de dados via FTP. Trabalhei com tecnologias antigas como o Net Framework 3.0 e 3.5, Visual Studio 2005 e 2008 e ainda Visual Basic 6, mas essa experiência foi fundamental para meu crescimento como desenvolvedor de software."
+			],
+			tags: [
+				"Visual C#",
+				"VB 6",
+				"NET Framework 3.5",
+				"Visual Source Safe",
+				"FTP",
+				"Expressões Regulares",
+			],
+			icon: "envelope-o",
+		},
+	],
+	education: [
+		{
+			title: "Bacharél em Análise e Desenvolvimento de Sistemas",
+			duration: "",
+			subtitle: "Fauldade de Tecnologia do Estado de SP, FATEC",
+			details: [
+				"Sou bacharel em Análise e Desenvolvimento de Sistemas pela Faculdade de Tecnologia do Estado de São Paulo, uma instituição pública de ensino de  renome. Durante minha graduação, tive a oportunidade de estudar uma ampla gama de disciplinas, incluindo Programação, Algoritmos, Laboratório de Hardware, Matemática Discreta, Engenharia de Software, Linguagem de Programação, Sistemas de Informação, Cálculo, Estrutura de Dados, Estatística Aplicada, Banco de Dados, Segurança da Informação, Redes de Computadores, Sistemas Operacionais e Gestão de Equipes.",
+				"Minha formação em Análise e Desenvolvimento de Sistemas não somente me permitiu adquirir habilidades valiosas para atuar como desenvolvedor de software, mas também ampliou  minha visão e compreensão sobre as complexidades e possibilidades do desenvolvimento de software."
+			],
+			tags: [
+				"Sistemas Operacionais",
+				"Engenharia de Software",
+				"Bancos de Dados",
+				"Testes de Software",
+				"Estrutura de Dados &amp; Algoritímos"
+			],
+			icon: "graduation-cap",
+		},
+		{
+			title: "Ensino Médio",
+			duration: "",
+			subtitle: "Colégio Campos Sales",
+			details: [
+				"Tive a oportunidade de estudar no ensino médio no Colégio Campos Salles, localizado no bairro da Lapa em São Paulo. Uma instituição de ensino tradicional, que me forneceu uma base sólida para continuar meus estudos e desenvolver minhas habilidades."
+			],
+			tags: [
+				"Português",
+				"Matemática",
+				"História",
+				"Geografia"
+			],
+			icon: "book",
+		},
+	],
+	courses: [
+		{
+			title: "Udemy",
+			duration: "15h",
+			subtitle: "",
+			details: [
+				"Postman: The Complete Guide - REST API Testing"
+			],
+			tags: [
+				"Postman",
+				"Newman",
+				"Testes de Software"
+			],
+			icon: "code",
+		},
+		{
+			title: "Azure Na Prática",
+			duration: "96 h",
+			subtitle: "",
+			details: [
+				"Azure DevOps",
+				"Github Actions"
+			],
+			tags: [
+				"Azure",
+				"DevOps",
+				"Github Actions",
+				"CI/CD"
+			],
+			icon: "code",
+		},
+		{
+			title: "Desenvolvedor.IO",
+			duration: "123 h",
+			subtitle: "",
+			details: [
+				"Formação Arquiteto de Software",
+				"Formação ASP.NET Core Expert"
+			],
+			tags: [
+				"NET Core",
+				"ASP.NET",
+				"TDD",
+				"BDD",
+				"CQRS",
+				"Mensageria"
+			],
+			icon: "code",
+		},
+		{
+			title: "Cisco",
+			duration: "70 h",
+			subtitle: "",
+			details: [
+				"IT Essentials"
+			],
+			tags: [
+				"Redes",
+				"Segurança",
+				"Sistemas Operacionais"
+			],
+			icon: "code",
+		},
+		{
+			title: "Oregon EAD",
+			duration: "",
+			subtitle: "",
+			details: [
+				"ASP.NET MVC4",
+				"Entity Framework 6"
+			],
+			tags: [
+				"ORM",
+				"Entity Framework",
+				"Bancos de Dados",
+				"ASP.NET",
+				"NET Framework"
+			],
+			icon: "code",
+		},
+		{
+			title: "Impacta Tecnologia",
+			duration: "256 h",
+			subtitle: "",
+			details: [
+				"Análise e Desenvolvimento de Sistemas – No 941409-101484",
+				"Introdução à POO – No 941409-101485",
+				"SQL 2008- Módulo I – No 941409-103000",
+				"C# 2010 - Módulo I – No 941409-103001",
+				"C# 2010 - Módulo II – No 941409-103002",
+				"ASP.NET 2010 com C# - Módulo I – No 941409-106075",
+				"ASP.NET 2010 com C# - Módulo II – No 941409-106075"
+			],
+			tags: [
+				"POO",
+				"NET Framework",
+				"ASP.NET",
+				"SQL Server"
+			],
+			icon: "code",
+		},
+	],
+	footer: [
+		{
+			label: "Dev Profiles",
+			data: [
+				{
+					text: "GitHub",
+					link: "https://github.com/willianrattis",
+				},
+				{
+					text: "LeetCode",
+					link: "https://leetcode.com//",
+				},
+			],
+		},
+		{
+			label: "Recursos",
+			data: [
+				{
+					text: "Habilitar Dark/Light Mode",
+					func: "enableDarkMode()",
+				},
+				{
+					text: "Imprima está página",
+					func: "window.print()",
+				}
+			],
+		},
+		{
+			label: "Social Profiles",
+			data: [
+				{
+					text: "LinkedIn",
+					link: "https://www.linkedin.com/in/willianrattis/",
+				}
+			],
+		},
+		{
+			label: "copyright-text",
+			data: [
+				"Feito com &hearts; ."
+			],
+		},
+	],
+};
